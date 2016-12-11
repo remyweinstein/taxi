7e3560938ef4aded044d9103501b8a53067b8796
dgreb <dgreb@192.168.15.104>
2016-12-11 13:02:01 +1000

    11.12.2016(h)

--- src/js/models/driver_orders.js
+++ src/js/models/driver_orders.js
@@ -32,7 +32,8 @@ define(['Dates'], function(Dates) {
 
                   self.id = order.id;
                   self.price = Old_Orders && Old_Orders !== "undefined" ? Old_Orders.price : Math.round(order.price);
-                  var travelTime = (order.agent.distance / average_speed).toFixed(0);
+                  self.distance2 = order.agent.distance ? order.agent.distance.toFixed(1) : 0;
+                  var travelTime = (self.distance2 / average_speed).toFixed(0);
                     if (travelTime < 5) {
                       travelTime = 5;
                     } else {
@@ -41,7 +42,6 @@ define(['Dates'], function(Dates) {
 
                   self.travelTime = Old_Orders && Old_Orders !== "undefined" ? Old_Orders.travelTime : travelTime;
                   self.stops = self.toAddresses.length ? self.toAddresses.length : 0;
-                  self.distance2 = order.agent.distance ? order.agent.distance.toFixed(1) : 0;
                   self.distance = order.distance ? order.distance : 0;
                   self.name = order.agent.name ? order.agent.name : User.default_name;
                   self.created = Dates.datetimeForPeople(order.created, 'LEFT_TIME_OR_DATE');
